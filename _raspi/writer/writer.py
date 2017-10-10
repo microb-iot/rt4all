a60a124916d923534d3227b2f81bf94c74c2c3b0
lobolanja <lobolanja@gmail.com>
2017-10-10 21:38:58 +0200

    writer whit ip

--- _raspi/writer/writer.py
+++ _raspi/writer/writer.py
@@ -11,15 +11,26 @@ from time import sleep
 filepath = osPath.dirname(osPath.realpath(__file__))
 sysPath.append(filepath + "/../")
 import rticonnextdds_connector as rti
+import socket
+import fcntl
+import struct
 
-connector = rti.Connector("MyParticipantLibrary::Zero",
-                          filepath + "/../XML/ShapeExample.xml")
-outputDDS = connector.getOutput("MyPublisher::MySquareWriter")
+def get_ip_address(ifname):
+	s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
 
+
+
+connector = rti.Connector("MyParticipantLibrary::Infinity",
+                          filepath + "/../XML/machine.xml")
+outputDDS = connector.getOutput("MyPublisher::MyMachineWriter")
+equipo = socket.gethostname()
+print equipo
+machine_ip=get_ip_address('wlp3s0')
+print machine_ip
 for i in range(1, 500):
-    outputDDS.instance.setNumber("x", i)
-    outputDDS.instance.setNumber("y", i*2)
-    outputDDS.instance.setNumber("shapesize", 30)
-    outputDDS.instance.setString("color", "BLUE")
+    outputDDS.instance.setString("machine", "robot")
+    outputDDS.instance.setNumber("machine_id", 0)
+    outputDDS.instance.setString("machine_ip", machine_ip)
     outputDDS.write()
     sleep(1)
