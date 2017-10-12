0898c2a22321ca4a167daf4dac6c7dc592fed5bf
LuciaAlvarez <luciaalvarez.gr@gmail.com>
2017-10-12 19:31:43 +0200

    log file improved

--- _video/interfaz/report/logger.py
+++ _video/interfaz/report/logger.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 from reportlab.graphics.shapes import Drawing, String
 from reportlab.graphics import renderPDF
 from reportlab.pdfgen import canvas
@@ -15,19 +16,30 @@ def graphic(variableGrafica, dataArray, initTime, interval):
 		print timeArray
 
 		#plot
-   		plt.xlabel('Time')
+   		plt.xlabel('time')
 		plt.ylabel(variableGrafica + ' samples')
 		plt.title(variableGrafica)
-		plt.plot(timeArray, dataArray, 'k')
+		plt.plot(timeArray, dataArray, 'ro', timeArray, dataArray, 'g')
 		plt.savefig('/var/tmp/'+variableGrafica+'.png')
-		plt.show()
+		#plt.show()
 		
 
 		
 
-def createPDF():
+def createPDF(user, machine, id, ip):
+
+
 		#generate a PDF
 		aux = canvas.Canvas("./pdf/LAG.pdf")
+		aux.drawImage("./logo.png",400, 750, 200,100)
+		aux.drawString(50,800,'****REPORT FILE****')
+		aux.drawString(50,750,'Date: '+ time.strftime("%d/%m/%y"))
+		aux.drawString(50,730,'Report time: ' +time.strftime("%H:%M:%S"))
+		aux.drawString(50,710,'Worker: ' +user)
+		aux.drawString(50,690,'Machine: ' +machine + '      ID: ' +id+'      IP: ' +ip)
+	
+		#aux.drawString(150,20,"Posicion (X,Y) = (150,20)")
+
 
 		aux.drawString(50,50,"RT4ALL logger")
 		#include graphics
@@ -45,10 +57,10 @@ def createPDF():
 if __name__ == "__main__":
 
 	#variableGrafica = input()
-	dataArray = [23,54,36,78, 32,76,12]
+	dataArray = [23,54,36,78, 32,76,12, 23,54,36,78, 32,76,12]
 	graphic('Temperature',dataArray, 1, 0.5)
 	graphic('Humidity',dataArray, 1, 0.5)
-	createPDF();
+	createPDF('Lucia', 'robot1', 'id1', '192.168.1.1');
 	
 	
 	
